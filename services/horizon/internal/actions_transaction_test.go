sreuland/go

commit 87c57e20646762f38ba9a7cfab241c55fd14585e

services/horizon: Cleanup routes in webserver init code (#2152)
This commit cleans the code responsible for Horizon webserver init.

* Moved all routes using the `StateMiddleware` (previously
`ExperimentalIngestionMiddleware`) to avoid code duplication.
* Start routes using order book graph only if the instance is ingesting.

services/horizon/internal/web.go
@@ -128,7 +128,7 @@ func (w *web) mustInstallActions(
 	config Config,
 	pathFinder paths.Finder,
 	orderBookGraph *orderbook.OrderBookGraph,
-	requiresExperimentalIngestion *ExperimentalIngestionMiddleware,
+	stateMiddleware *StateMiddleware,
 ) {
 	if w == nil {
 		log.Fatal("missing web instance for installing web actions")
@@ -137,6 +137,75 @@ func (w *web) mustInstallActions(
 	r := w.router
 	r.Get("/", RootAction{}.Handle)
 	r.Get("/metrics", MetricsAction{}.Handle)
+
+	streamHandler := sse.StreamHandler{
+		RateLimiter:  w.rateLimiter,
+		LedgerSource: ledger.NewHistoryDBSource(w.sseUpdateFrequency),
+	}
+
+	// State endpoints behind stateMiddleware
+	r.Group(func(r chi.Router) {
+		r.Use(stateMiddleware.Wrap)
+
+		r.Route("/accounts", func(r chi.Router) {
+			r.Method(http.MethodGet, "/", restPageHandler(actions.GetAccountsHandler{}))
+			r.Route("/{account_id}", func(r chi.Router) {
+				r.Get("/", w.streamShowActionHandler(w.getAccountInfo, true))
+				r.Get("/data/{key}", DataShowAction{}.Handle)
+				r.Method(http.MethodGet, "/offers", streamablePageHandler(actions.GetAccountOffersHandler{}, streamHandler))
+			})
+		})
+
+		r.Route("/offers", func(r chi.Router) {
+			r.Method(http.MethodGet, "/", restPageHandler(actions.GetOffersHandler{}))
+			r.Method(http.MethodGet, "/{id}", objectActionHandler{actions.GetOfferByID{}})
+		})
+
+		r.Method(http.MethodGet, "/assets", restPageHandler(actions.AssetStatsHandler{}))
+
+		// Endpoints available on ingesting instances only.
+		if config.Ingest {
+			findPaths := FindPathsHandler{
+				staleThreshold:       config.StaleThreshold,
+				checkHistoryIsStale:  false,
+				setLastLedgerHeader:  true,
+				maxPathLength:        config.MaxPathLength,
+				maxAssetsParamLength: maxAssetsForPathFinding,
+				pathFinder:           pathFinder,
+				coreQ:                w.coreQ,
+			}
+			findFixedPaths := FindFixedPathsHandler{
+				maxPathLength:        config.MaxPathLength,
+				setLastLedgerHeader:  true,
+				maxAssetsParamLength: maxAssetsForPathFinding,
+				pathFinder:           pathFinder,
+				coreQ:                w.coreQ,
+			}
+
+			r.Method(http.MethodGet, "/paths", findPaths)
+			r.Method(http.MethodGet, "/paths/strict-receive", findPaths)
+			r.Method(http.MethodGet, "/paths/strict-send", findFixedPaths)
+
+			r.Method(
+				http.MethodGet,
+				"/order_book",
+				streamableObjectActionHandler{
+					streamHandler: streamHandler,
+					action: actions.GetOrderbookHandler{
+						OrderBookGraph: orderBookGraph,
+					},
+				},
+			)
+		}
+	})
+
+	// account actions - /accounts/{account_id} has been created above so we
+	// need to use absolute routes here.
+	r.Get("/accounts/{account_id}/transactions", w.streamIndexActionHandler(w.getTransactionPage, w.streamTransactions))
+	r.Get("/accounts/{account_id}/operations", OperationIndexAction{}.Handle)
+	r.Get("/accounts/{account_id}/payments", OperationIndexAction{OnlyPayments: true}.Handle)
+	r.Get("/accounts/{account_id}/effects", EffectIndexAction{}.Handle)
+	r.Get("/accounts/{account_id}/trades", TradeIndexAction{}.Handle)
 
 	// ledger actions
 	r.Route("/ledgers", func(r chi.Router) {
@@ -149,36 +218,6 @@ func (w *web) mustInstallActions(
 			r.Get("/effects", EffectIndexAction{}.Handle)
 		})
 	})
-
-	// account actions
-	r.Route("/accounts", func(r chi.Router) {
-		r.With(requiresExperimentalIngestion.Wrap).
-			Method(
-				http.MethodGet,
-				"/",
-				restPageHandler(actions.GetAccountsHandler{}),
-			)
-		r.Route("/{account_id}", func(r chi.Router) {
-			r.Get("/", w.streamShowActionHandler(w.getAccountInfo, true))
-			r.Get("/transactions", w.streamIndexActionHandler(w.getTransactionPage, w.streamTransactions))
-			r.Get("/operations", OperationIndexAction{}.Handle)
-			r.Get("/payments", OperationIndexAction{OnlyPayments: true}.Handle)
-			r.Get("/effects", EffectIndexAction{}.Handle)
-			r.Get("/trades", TradeIndexAction{}.Handle)
-			r.Get("/data/{key}", DataShowAction{}.Handle)
-		})
-	})
-
-	streamHandler := sse.StreamHandler{
-		RateLimiter:  w.rateLimiter,
-		LedgerSource: ledger.NewHistoryDBSource(w.sseUpdateFrequency),
-	}
-
-	r.With(requiresExperimentalIngestion.Wrap).Method(
-		http.MethodGet,
-		"/accounts/{account_id}/offers",
-		streamablePageHandler(actions.GetAccountOffersHandler{}, streamHandler),
-	)
 
 	// transaction history actions
 	r.Route("/transactions", func(r chi.Router) {
@@ -207,67 +246,12 @@ func (w *web) mustInstallActions(
 	// trading related endpoints
 	r.Get("/trades", TradeIndexAction{}.Handle)
 	r.Get("/trade_aggregations", TradeAggregateIndexAction{}.Handle)
-
-	r.Route("/offers", func(r chi.Router) {
-		r.With(requiresExperimentalIngestion.Wrap).
-			Method(
-				http.MethodGet,
-				"/",
-				restPageHandler(actions.GetOffersHandler{}),
-			)
-		r.With(acceptOnlyJSON, requiresExperimentalIngestion.Wrap).
-			Method(
-				http.MethodGet,
-				"/{id}",
-				objectActionHandler{actions.GetOfferByID{}},
-			)
-		r.Get("/{offer_id}/trades", TradeIndexAction{}.Handle)
-	})
-
-	r.With(requiresExperimentalIngestion.Wrap).Method(
-		http.MethodGet,
-		"/order_book",
-		streamableObjectActionHandler{
-			streamHandler: streamHandler,
-			action: actions.GetOrderbookHandler{
-				OrderBookGraph: orderBookGraph,
-			},
-		},
-	)
+	// /offers/{offer_id} has been created above so we need to use absolute
+	// routes here.
+	r.Get("/offers/{offer_id}/trades", TradeIndexAction{}.Handle)
 
 	// Transaction submission API
 	r.Post("/transactions", TransactionCreateAction{}.Handle)
-
-	findPaths := FindPathsHandler{
-		staleThreshold:       config.StaleThreshold,
-		checkHistoryIsStale:  false,
-		setLastLedgerHeader:  true,
-		maxPathLength:        config.MaxPathLength,
-		maxAssetsParamLength: maxAssetsForPathFinding,
-		pathFinder:           pathFinder,
-		coreQ:                w.coreQ,
-	}
-	findFixedPaths := FindFixedPathsHandler{
-		maxPathLength:        config.MaxPathLength,
-		setLastLedgerHeader:  true,
-		maxAssetsParamLength: maxAssetsForPathFinding,
-		pathFinder:           pathFinder,
-		coreQ:                w.coreQ,
-	}
-
-	r.Group(func(r chi.Router) {
-		r.Use(acceptOnlyJSON)
-		r.Use(requiresExperimentalIngestion.Wrap)
-		r.Method("GET", "/paths", findPaths)
-		r.Method("GET", "/paths/strict-receive", findPaths)
-		r.Method("GET", "/paths/strict-send", findFixedPaths)
-	})
-
-	r.With(requiresExperimentalIngestion.Wrap).Method(
-		http.MethodGet,
-		"/assets",
-		restPageHandler(actions.AssetStatsHandler{}),
-	)
 
 	// Network state related endpoints
 	r.Get("/fee_stats", FeeStatsAction{}.Handle)

services/horizon/internal/actions/helpers.go
@@ -690,6 +690,9 @@ func GetParams(dst interface{}, r *http.Request) error {
 	// contain `account_id` and URL Query params will contain `foo`.
 	if rctx := chi.RouteContext(r.Context()); rctx != nil {
 		for _, key := range rctx.URLParams.Keys {
+			if key == "*" {
+				continue
+			}
 			val := query.Get(key)
 			if len(val) > 0 {
 				return problem.MakeInvalidFieldProblem(

services/horizon/internal/actions_account_test.go
@@ -2,11 +2,28 @@ package horizon
 
 import (
 	"testing"
+
+	"github.com/stellar/go/services/horizon/internal/db2/history"
+	"github.com/stellar/go/services/horizon/internal/expingest"
+	"github.com/stellar/go/xdr"
 )
 
 func TestAccountActions_InvalidID(t *testing.T) {
 	ht := StartHTTPTestWithoutScenario(t)
 	defer ht.Finish()
+
+	// Makes StateMiddleware happy
+	q := history.Q{ht.HorizonSession()}
+	err := q.UpdateLastLedgerExpIngest(100)
+	ht.Assert.NoError(err)
+	err = q.UpdateExpIngestVersion(expingest.CurrentVersion)
+	ht.Assert.NoError(err)
+	_, err = q.InsertLedger(xdr.LedgerHeaderHistoryEntry{
+		Header: xdr.LedgerHeader{
+			LedgerSeq: 100,
+		},
+	}, 0, 0, 0, 0)
+	ht.Assert.NoError(err)
 
 	// existing account
 	w := ht.Get(

services/horizon/internal/actions_data_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stellar/go/services/horizon/internal/db2/history"
+	"github.com/stellar/go/services/horizon/internal/expingest"
 	"github.com/stellar/go/services/horizon/internal/test"
 	"github.com/stellar/go/xdr"
 	"github.com/stretchr/testify/assert"
@@ -31,6 +32,18 @@ func TestDataActions_Show(t *testing.T) {
 	defer ht.Finish()
 	test.ResetHorizonDB(t, ht.HorizonDB)
 	q := &history.Q{ht.HorizonSession()}
+
+	// Makes StateMiddleware happy
+	err := q.UpdateLastLedgerExpIngest(100)
+	ht.Assert.NoError(err)
+	err = q.UpdateExpIngestVersion(expingest.CurrentVersion)
+	ht.Assert.NoError(err)
+	_, err = q.InsertLedger(xdr.LedgerHeaderHistoryEntry{
+		Header: xdr.LedgerHeader{
+			LedgerSeq: 100,
+		},
+	}, 0, 0, 0, 0)
+	ht.Assert.NoError(err)
 
 	rows, err := q.InsertAccountData(data1, 1234)
 	assert.NoError(t, err)

services/horizon/internal/actions_path_test.go
@@ -42,10 +42,6 @@ func inMemoryPathFindingClient(
 	}
 
 	router.Group(func(r chi.Router) {
-		// requiresExperimentalIngestion := &ExperimentalIngestionMiddleware{
-		// 	HorizonSession: tt.HorizonSession(),
-		// }
-		// router.Use(requiresExperimentalIngestion.Wrap)
 		router.Method("GET", "/paths", findPaths)
 		router.Method("GET", "/paths/strict-receive", findPaths)
 		router.Method("GET", "/paths/strict-send", findFixedPaths)
@@ -77,10 +73,6 @@ func dbPathFindingClient(
 	}
 
 	router.Group(func(r chi.Router) {
-		// requiresExperimentalIngestion := &ExperimentalIngestionMiddleware{
-		// 	HorizonSession: tt.HorizonSession(),
-		// }
-		// router.Use(requiresExperimentalIngestion.Wrap)
 		router.Method("GET", "/paths", findPaths)
 		router.Method("GET", "/paths/strict-receive", findPaths)
 		router.Method("GET", "/paths/strict-send", findFixedPaths)

services/horizon/internal/app.go
@@ -462,7 +462,7 @@ func (a *App) init() {
 	// This parameter will be removed soon.
 	a.web.mustInstallMiddlewares(a, a.config.ConnectionTimeout)
 
-	requiresExperimentalIngestion := &ExperimentalIngestionMiddleware{
+	requiresExperimentalIngestion := &StateMiddleware{
 		HorizonSession: a.historyQ.Session,
 	}
 	// web.actions

services/horizon/internal/middleware.go
@@ -241,26 +241,12 @@ func requestMetricsMiddleware(h http.Handler) http.Handler {
 	})
 }
 
-// acceptOnlyJSON inspects the accept header of the request and responds with
-// an error if the content type is not JSON
-func acceptOnlyJSON(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		contentType := render.Negotiate(r)
-		if contentType != render.MimeHal && contentType != render.MimeJSON {
-			problem.Render(r.Context(), w, hProblem.NotAcceptable)
-			return
-		}
-
-		h.ServeHTTP(w, r)
-	})
-}
-
-// ExperimentalIngestionMiddleware is a middleware which enables a handler
-// if the experimental ingestion system is enabled and initialized.
+// StateMiddleware is a middleware which enables a state handler if the state
+// has been initialized.
 // It also ensures that state (ledger entries) has been verified and are
 // correct. Otherwise returns `500 Internal Server Error` to prevent
 // returning invalid data to the user.
-type ExperimentalIngestionMiddleware struct {
+type StateMiddleware struct {
 	HorizonSession *db.Session
 }
 
@@ -293,7 +279,7 @@ func ingestionStatus(q *history.Q) (uint32, bool, error) {
 }
 
 // Wrap executes the middleware on a given http handler
-func (m *ExperimentalIngestionMiddleware) Wrap(h http.Handler) http.Handler {
+func (m *StateMiddleware) Wrap(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		session := m.HorizonSession.Clone()
 		q := &history.Q{session}

services/horizon/internal/middleware_test.go
@@ -152,7 +152,7 @@ func TestRateLimit_Redis(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 }
 
-func TestRequiresExperimentalIngestion(t *testing.T) {
+func TestStateMiddleware(t *testing.T) {
 	tt := test.Start(t)
 	defer tt.Finish()
 	test.ResetHorizonDB(t, tt.HorizonDB)
@@ -173,10 +173,10 @@ func TestRequiresExperimentalIngestion(t *testing.T) {
 		w.WriteHeader(http.StatusOK)
 	}
 
-	requiresExperimentalIngestion := &ExperimentalIngestionMiddleware{
+	stateMiddleware := &StateMiddleware{
 		HorizonSession: tt.HorizonSession(),
 	}
-	handler := requiresExperimentalIngestion.Wrap(http.HandlerFunc(endpoint))
+	handler := stateMiddleware.Wrap(http.HandlerFunc(endpoint))
 
 	for i, testCase := range []struct {
 		name                string

commit c0a852cc94baa5ba904e42ed1f74e158fc497891

services/horizon: Fix empty route param bug (#2155)
Fixes a bug introduced in 87c57e2. Paths like: `/accounts//payments`
return all payments instead of 404. This can lead to dangerous bugs when
account ID is omited in request by accident.

services/horizon/internal/web.go
@@ -200,12 +200,13 @@ func (w *web) mustInstallActions(
 	})
 
 	// account actions - /accounts/{account_id} has been created above so we
-	// need to use absolute routes here.
-	r.Get("/accounts/{account_id}/transactions", w.streamIndexActionHandler(w.getTransactionPage, w.streamTransactions))
-	r.Get("/accounts/{account_id}/operations", OperationIndexAction{}.Handle)
-	r.Get("/accounts/{account_id}/payments", OperationIndexAction{OnlyPayments: true}.Handle)
-	r.Get("/accounts/{account_id}/effects", EffectIndexAction{}.Handle)
-	r.Get("/accounts/{account_id}/trades", TradeIndexAction{}.Handle)
+	// need to use absolute routes here. Make sure we use regexp check here for
+	// emptiness. Without it, requesting `/accounts//payments` return all payments!
+	r.Get("/accounts/{account_id:\\w+}/transactions", w.streamIndexActionHandler(w.getTransactionPage, w.streamTransactions))
+	r.Get("/accounts/{account_id:\\w+}/operations", OperationIndexAction{}.Handle)
+	r.Get("/accounts/{account_id:\\w+}/payments", OperationIndexAction{OnlyPayments: true}.Handle)
+	r.Get("/accounts/{account_id:\\w+}/effects", EffectIndexAction{}.Handle)
+	r.Get("/accounts/{account_id:\\w+}/trades", TradeIndexAction{}.Handle)
 
 	// ledger actions
 	r.Route("/ledgers", func(r chi.Router) {

services/horizon/internal/actions_effects_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stellar/go/protocols/horizon/effects"
 	"github.com/stellar/go/services/horizon/internal/db2/history"
+	"github.com/stellar/go/services/horizon/internal/expingest"
 	"github.com/stellar/go/services/horizon/internal/test"
 )
 
@@ -39,6 +40,17 @@ func TestEffectActions_Index(t *testing.T) {
 		if ht.Assert.Equal(200, w.Code) {
 			ht.Assert.PageOf(2, w.Body)
 		}
+
+		// Makes StateMiddleware happy
+		q := history.Q{ht.HorizonSession()}
+		err := q.UpdateLastLedgerExpIngest(3)
+		ht.Assert.NoError(err)
+		err = q.UpdateExpIngestVersion(expingest.CurrentVersion)
+		ht.Assert.NoError(err)
+
+		// checks if empty param returns 404 instead of all payments
+		w = ht.Get("/accounts//effects")
+		ht.Assert.NotEqual(404, w.Code)
 
 		// filtered by account
 		w = ht.Get("/accounts/GBRPYHIL2CI3FNQ4BXLFMNDLFJUNPU2HY3ZMFSHONUCEOASW7QC7OX2H/effects")

services/horizon/internal/actions_payment_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/protocols/horizon/operations"
 	"github.com/stellar/go/services/horizon/internal/db2/history"
+	"github.com/stellar/go/services/horizon/internal/expingest"
 )
 
 func TestPaymentActions(t *testing.T) {
@@ -29,6 +30,17 @@ func TestPaymentActions(t *testing.T) {
 	if ht.Assert.Equal(200, w.Code) {
 		ht.Assert.PageOf(1, w.Body)
 	}
+
+	// Makes StateMiddleware happy
+	q := history.Q{ht.HorizonSession()}
+	err := q.UpdateLastLedgerExpIngest(3)
+	ht.Assert.NoError(err)
+	err = q.UpdateExpIngestVersion(expingest.CurrentVersion)
+	ht.Assert.NoError(err)
+
+	// checks if empty param returns 404 instead of all payments
+	w = ht.Get("/accounts//payments")
+	ht.Assert.NotEqual(404, w.Code)
 
 	// filtered by account
 	w = ht.Get("/accounts/GA5WBPYA5Y4WAEHXWR2UKO2UO4BUGHUQ74EUPKON2QHV4WRHOIRNKKH2/payments")

services/horizon/internal/actions_transaction_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 
 	"github.com/stellar/go/protocols/horizon"
+	"github.com/stellar/go/services/horizon/internal/db2/history"
+	"github.com/stellar/go/services/horizon/internal/expingest"
 	"github.com/stellar/go/services/horizon/internal/txsub"
 	"github.com/stellar/go/services/horizon/internal/txsub/sequence"
 )
@@ -178,6 +180,17 @@ func TestTransactionActions_Index(t *testing.T) {
 	// missing ledger
 	w = ht.Get("/ledgers/100/transactions")
 	ht.Assert.Equal(404, w.Code)
+
+	// Makes StateMiddleware happy
+	q := history.Q{ht.HorizonSession()}
+	err := q.UpdateLastLedgerExpIngest(100)
+	ht.Assert.NoError(err)
+	err = q.UpdateExpIngestVersion(expingest.CurrentVersion)
+	ht.Assert.NoError(err)
+
+	// checks if empty param returns 404 instead of all payments
+	w = ht.Get("/accounts//transactions")
+	ht.Assert.NotEqual(404, w.Code)
 
 	// filtering by account
 	w = ht.Get("/accounts/GBRPYHIL2CI3FNQ4BXLFMNDLFJUNPU2HY3ZMFSHONUCEOASW7QC7OX2H/transactions")