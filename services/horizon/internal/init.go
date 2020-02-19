sreuland/go

commit 480d73cc5865b9bc0955586edc64a417ff561a4f

services/horizon/txsub: Skip checking Core DB if Horizon is ingesting failed txs (#2277)
Skip checking Stellar-Core DB for transaction results if Horizon is
ingesting failed transactions.

Due to a delay between Stellar-Core closing a ledger and Horizon
ingesting it, it's possible that results of transaction submitted to
Stellar-Core via Horizon may not be immediately visible. This is
happening because `txsub` package checks two sources when checking a
transaction result: Stellar-Core and Horizon DB.

The extreme case is https://github.com/stellar/go/issues/2272 where
results of transaction creating an account are not visible: requesting
account details in Horizon returns `404 Not Found`:

     Horizon DB                  Core DB                  User
         |                          |                      |
         |                          |                      |
         |                          | <------- Submit create_account op
         |                          |                      |
         |                  Insert transaction             |
         |                          |                      |
         |                     Tx found -----------------> |
         |                          |                      |
         |                          |                      |
         | <--------------------------------------- Get account info
         |                          |                      |
         |                          |                      |
Account NOT found ---------------------------------------> |
         |                          |                      |
   Insert account                   |                      |

services/horizon/internal/init.go
@@ -198,7 +198,41 @@ func dialRedis(redisURL *url.URL) func() (redis.Conn, error) {
 }
 
 func initSubmissionSystem(app *App) {
-	cq := &core.Q{Session: app.CoreSession(context.Background())}
+	// Due to a delay between Stellar-Core closing a ledger and Horizon
+	// ingesting it, it's possible that results of transaction submitted to
+	// Stellar-Core via Horizon may not be immediately visible. This is
+	// happening because `txsub` package checks two sources when checking a
+	// transaction result: Stellar-Core and Horizon DB.
+	//
+	// The extreme case is https://github.com/stellar/go/issues/2272 where
+	// results of transaction creating an account are not visible: requesting
+	// account details in Horizon returns `404 Not Found`:
+	//
+	// ```
+	// 	 Horizon DB                  Core DB                  User
+	// 		 |                          |                      |
+	// 		 |                          |                      |
+	// 		 |                          | <------- Submit create_account op
+	// 		 |                          |                      |
+	// 		 |                  Insert transaction             |
+	// 		 |                          |                      |
+	// 		 |                     Tx found -----------------> |
+	// 		 |                          |                      |
+	// 		 |                          |                      |
+	// 		 | <--------------------------------------- Get account info
+	// 		 |                          |                      |
+	// 		 |                          |                      |
+	// Account NOT found ---------------------------------------> |
+	// 		 |                          |                      |
+	//    Insert account                   |                      |
+	// ```
+	//
+	// To fix this Skip checking Stellar-Core DB for transaction results if
+	// Horizon is ingesting failed transactions.
+	var cq *core.Q
+	if !app.config.IngestFailedTransactions {
+		cq = &core.Q{Session: app.CoreSession(context.Background())}
+	}
 
 	app.submitter = &txsub.System{
 		Pending:         txsub.NewDefaultSubmissionList(),

services/horizon/internal/txsub/results/db/main.go
@@ -14,8 +14,8 @@ import (
 	"github.com/stellar/go/xdr"
 )
 
-// DB provides transactio submission results by querying the
-// connected horizon and stellar core databases.
+// DB provides transaction submission results by querying the
+// connected horizon and, if set, stellar core databases.
 type DB struct {
 	Core    *core.Q
 	History *history.Q
@@ -38,26 +38,28 @@ func (rp *DB) ResultByHash(ctx context.Context, hash string) txsub.Result {
 		return txsub.Result{Err: err}
 	}
 
-	// query core database
-	var cr core.Transaction
-	// In the past we were searching for the transaction in core DB *after* the
-	// latest ingested ledger. This was incorrect because history DB contains
-	// successful transactions only. So it was possible that the transaction was
-	// never found and clients were receiving Timeout errors.
-	// However we can't change it to simply find a transaction by hash because
-	// `txhistory` table does not have an index on `txid` field. Because of this
-	// we query the last 120 ledgers (~10 minutes) to not kill the DB by searching
-	// for a value on a table with millions of rows but also to support returning
-	// the failed tx result (when resubmitting) for 10 minutes (or before core
-	// clears `txhistory` table, whatever is first).
-	// If you are modifying the code here, please do not make this error again.
-	err = rp.Core.TransactionByHashAfterLedger(&cr, hash, historyLatest-120)
-	if err == nil {
-		return txResultFromCore(cr)
-	}
+	if rp.Core != nil {
+		// query core database
+		var cr core.Transaction
+		// In the past we were searching for the transaction in core DB *after* the
+		// latest ingested ledger. This was incorrect because history DB contains
+		// successful transactions only. So it was possible that the transaction was
+		// never found and clients were receiving Timeout errors.
+		// However we can't change it to simply find a transaction by hash because
+		// `txhistory` table does not have an index on `txid` field. Because of this
+		// we query the last 120 ledgers (~10 minutes) to not kill the DB by searching
+		// for a value on a table with millions of rows but also to support returning
+		// the failed tx result (when resubmitting) for 10 minutes (or before core
+		// clears `txhistory` table, whatever is first).
+		// If you are modifying the code here, please do not make this error again.
+		err = rp.Core.TransactionByHashAfterLedger(&cr, hash, historyLatest-120)
+		if err == nil {
+			return txResultFromCore(cr)
+		}
 
-	if !rp.Core.NoRows(err) {
-		return txsub.Result{Err: err}
+		if !rp.Core.NoRows(err) {
+			return txsub.Result{Err: err}
+		}
 	}
 
 	// if no result was found in either db, return ErrNoResults

services/horizon/internal/txsub/results/db/main_test.go
@@ -27,6 +27,24 @@ func TestResultProvider(t *testing.T) {
 	tt.Assert.Equal(hash, ret.Hash)
 }
 
+func TestResultProviderHorizonOnly(t *testing.T) {
+	tt := test.Start(t).Scenario("base")
+	defer tt.Finish()
+
+	rp := &DB{
+		History: &history.Q{Session: tt.HorizonSession()},
+	}
+
+	hash := "adf1efb9fd253f53cbbe6230c131d2af19830328e52b610464652d67d2fb7195"
+	_, err := tt.CoreSession().ExecRaw("INSERT INTO txhistory VALUES ('" + hash + "', 5, 1, 'AAAAAGL8HQvQkbK2HA3WVjRrKmjX00fG8sLI7m0ERwJW/AX3AAAAZAAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAArqN6LeOagjxMaUP96Bzfs9e0corNZXzBWJkFoK7kvkwAAAAAO5rKAAAAAAAAAAABVvwF9wAAAECDzqvkQBQoNAJifPRXDoLhvtycT3lFPCQ51gkdsFHaBNWw05S/VhW0Xgkr0CBPE4NaFV2Kmcs3ZwLmib4TRrML', 'I3Tpk0m57326ml2zM5t4/ajzR3exrzO6RorVwN+UbU0AAAAAAAAAZAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAA==', 'AAAAAQAAAAAAAAABAAAAAwAAAAMAAAACAAAAAAAAAABi/B0L0JGythwN1lY0aypo19NHxvLCyO5tBEcCVvwF9w3gtrOnY/7UAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAEAAAACAAAAAAAAAABi/B0L0JGythwN1lY0aypo19NHxvLCyO5tBEcCVvwF9w3gtrNryTTUAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAACuo3ot45qCPExpQ/3oHN+z17Ryis1lfMFYmQWgruS+TAAAAAA7msoAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAA==');")
+	tt.Require.NoError(err)
+
+	ret := rp.ResultByHash(tt.Ctx, hash)
+
+	tt.Require.Equal(ret.Err, txsub.ErrNoResults)
+	tt.Assert.Empty(ret.Hash)
+}
+
 func TestResultFailed(t *testing.T) {
 	tt := test.Start(t).Scenario("failed_transactions")
 	defer tt.Finish()

commit e661e96c3932bbfd09195fdac88f6486b92616d2

services/horizon/txsub: Use CoreDB for SequenceProvider (#2287)
This commit fixes a bug introduced in 480d73c. It adds `core.Q` back to
`txsub/results.DB` and uses a new `SkipCoreChecks` fields to decide if
core DB should be used or not.

`core.Q` is also needed to provide `SequenceProvider` to `txsub` system.
In the future we can use Horizon DB as a `SequenceProvider` but it
requires more tests.

services/horizon/internal/init.go
@@ -222,25 +222,23 @@ func initSubmissionSystem(app *App) {
 	// 		 | <--------------------------------------- Get account info
 	// 		 |                          |                      |
 	// 		 |                          |                      |
-	// Account NOT found ---------------------------------------> |
+	//         Account NOT found ------------------------------------> |
 	// 		 |                          |                      |
-	//    Insert account                   |                      |
+	//         Insert account                   |                      |
 	// ```
 	//
-	// To fix this Skip checking Stellar-Core DB for transaction results if
+	// To fix this skip checking Stellar-Core DB for transaction results if
 	// Horizon is ingesting failed transactions.
-	var cq *core.Q
-	if !app.config.IngestFailedTransactions {
-		cq = &core.Q{Session: app.CoreSession(context.Background())}
-	}
+	cq := &core.Q{Session: app.CoreSession(context.Background())}
 
 	app.submitter = &txsub.System{
 		Pending:         txsub.NewDefaultSubmissionList(),
 		Submitter:       txsub.NewDefaultSubmitter(http.DefaultClient, app.config.StellarCoreURL),
 		SubmissionQueue: sequence.NewManager(),
 		Results: &results.DB{
-			Core:    cq,
-			History: &history.Q{Session: app.HorizonSession(context.Background())},
+			Core:           &core.Q{Session: app.CoreSession(context.Background())},
+			History:        &history.Q{Session: app.HorizonSession(context.Background())},
+			SkipCoreChecks: app.config.IngestFailedTransactions,
 		},
 		Sequences:         cq.SequenceProvider(),
 		NetworkPassphrase: app.config.NetworkPassphrase,

services/horizon/internal/txsub/results/db/main.go
@@ -19,6 +19,8 @@ import (
 type DB struct {
 	Core    *core.Q
 	History *history.Q
+	// SkipCoreChecks makes DB skip checking transaction result in Core DB if `true`.
+	SkipCoreChecks bool
 }
 
 var _ txsub.ResultProvider = &DB{}
@@ -38,7 +40,7 @@ func (rp *DB) ResultByHash(ctx context.Context, hash string) txsub.Result {
 		return txsub.Result{Err: err}
 	}
 
-	if rp.Core != nil {
+	if !rp.SkipCoreChecks {
 		// query core database
 		var cr core.Transaction
 		// In the past we were searching for the transaction in core DB *after* the

services/horizon/internal/txsub/results/db/main_test.go
@@ -32,7 +32,9 @@ func TestResultProviderHorizonOnly(t *testing.T) {
 	defer tt.Finish()
 
 	rp := &DB{
-		History: &history.Q{Session: tt.HorizonSession()},
+		Core:           &core.Q{Session: tt.CoreSession()},
+		History:        &history.Q{Session: tt.HorizonSession()},
+		SkipCoreChecks: true,
 	}
 
 	hash := "adf1efb9fd253f53cbbe6230c131d2af19830328e52b610464652d67d2fb7195"