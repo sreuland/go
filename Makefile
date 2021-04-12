sreuland/go

commit 74bd0ffab86095cf8f05384f14823c47bc9979ee

xdr: Add random XDR generator (#3523)
This commit introduces the randxdr package to the monorepo. randxdr is used for generating XDR values with random data.

Makefile
@@ -1,6 +1,12 @@
 # Docker build targets use an optional "TAG" environment
 # variable can be set to use custom tag name. For example:
 #   TAG=my-registry.example.com/keystore:dev make keystore
+XDRS = xdr/Stellar-SCP.x \
+xdr/Stellar-ledger-entries.x \
+xdr/Stellar-ledger.x \
+xdr/Stellar-overlay.x \
+xdr/Stellar-transaction.x \
+xdr/Stellar-types.x
 
 keystore:
 	$(MAKE) -C services/keystore/ docker-build
@@ -19,3 +25,7 @@ recoverysigner:
 
 regulated-assets-approval-server:
 	$(MAKE) -C services/regulated-assets-approval-server/ docker-build
+
+gxdr/xdr_generated.go: $(XDRS)
+	go run github.com/xdrpp/goxdr/cmd/goxdr -p gxdr -enum-comments -o $@~ $(XDRS)
+	cmp $@~ $@ 2> /dev/null || mv -f $@~ $@

go.list
@@ -112,6 +112,7 @@ github.com/stretchr/testify v1.5.1
 github.com/tyler-smith/go-bip39 v0.0.0-20180618194314-52158e4697b8
 github.com/valyala/bytebufferpool v1.0.0
 github.com/valyala/fasthttp v0.0.0-20170109085056-0a7f0a797cd6
+github.com/xdrpp/goxdr v0.1.0
 github.com/xeipuuv/gojsonpointer v0.0.0-20151027082146-e0fe6f683076
 github.com/xeipuuv/gojsonreference v0.0.0-20150808065054-e02fc20de94c
 github.com/xeipuuv/gojsonschema v0.0.0-20161231055540-f06f290571ce

go.mod
@@ -70,6 +70,7 @@ require (
 	github.com/tyler-smith/go-bip39 v0.0.0-20180618194314-52158e4697b8
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v0.0.0-20170109085056-0a7f0a797cd6 // indirect
+	github.com/xdrpp/goxdr v0.1.0
 	github.com/xeipuuv/gojsonpointer v0.0.0-20151027082146-e0fe6f683076 // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20150808065054-e02fc20de94c // indirect
 	github.com/xeipuuv/gojsonschema v0.0.0-20161231055540-f06f290571ce // indirect

go.sum
@@ -214,6 +214,8 @@ github.com/valyala/bytebufferpool v1.0.0 h1:GqA5TC/0021Y/b9FG4Oi9Mr3q7XYx6Kllzaw
 github.com/valyala/bytebufferpool v1.0.0/go.mod h1:6bBcMArwyJ5K/AmCkWv1jt77kVWyCJ6HpOuEn7z0Csc=
 github.com/valyala/fasthttp v0.0.0-20170109085056-0a7f0a797cd6 h1:s0IDmR1jFyWvOK7jVIuAsmHQaGkXUuTas8NXFUOwuAI=
 github.com/valyala/fasthttp v0.0.0-20170109085056-0a7f0a797cd6/go.mod h1:+g/po7GqyG5E+1CNgquiIxJnsXEi5vwFn5weFujbO78=
+github.com/xdrpp/goxdr v0.1.0 h1:464WVKpe16VazriMaPxuGZtU/oE0Bxr5q5hLGs2I0rs=
+github.com/xdrpp/goxdr v0.1.0/go.mod h1:sIkGTrelHHneJXYd+dJGuziv4dWDofbdMl+onW+E3x8=
 github.com/xeipuuv/gojsonpointer v0.0.0-20151027082146-e0fe6f683076 h1:KM4T3G70MiR+JtqplcYkNVoNz7pDwYaBxWBXQK804So=
 github.com/xeipuuv/gojsonpointer v0.0.0-20151027082146-e0fe6f683076/go.mod h1:N2zxlSyiKSe5eX1tZViRH5QA0qijqEDrYZiPEAiq3wU=
 github.com/xeipuuv/gojsonreference v0.0.0-20150808065054-e02fc20de94c h1:XZWnr3bsDQWAZg4Ne+cPoXRPILrNlPNQfxBuwLl43is=

gogenerate.sh
@@ -13,5 +13,8 @@ command -v go-bindata >/dev/null 2>&1 || (
 printf "Running go generate...\n"
 go generate ./...
 
+printf "Running make gxdr/xdr_generated.go...\n"
+make gxdr/xdr_generated.go
+
 printf "Checking for no diff...\n"
 git diff --exit-code || (echo "Files changed after running go generate. Run go generate ./... locally and update generated files." && exit 1)

gxdr/dump.go
@@ -1,0 +1,25 @@
+package gxdr
+
+import (
+	"bytes"
+	"encoding"
+
+	goxdr "github.com/xdrpp/goxdr/xdr"
+)
+
+// Dump serializes the given goxdr value into binary.
+func Dump(v goxdr.XdrType) []byte {
+	var buf bytes.Buffer
+	writer := goxdr.XdrOut{Out: &buf}
+	writer.Marshal("", v)
+	return buf.Bytes()
+}
+
+// Convert serializes the given goxdr value into another destination value
+// which supports binary unmarshalling.
+//
+// This function can be used to convert github.com/xdrpp/goxdr/xdr values into
+// equivalent https://github.com/stellar/go-xdr values.
+func Convert(src goxdr.XdrType, dest encoding.BinaryUnmarshaler) error {
+	return dest.UnmarshalBinary(Dump(src))
+}

randxdr/generator.go
@@ -1,0 +1,52 @@
+package randxdr
+
+import (
+	"math/rand"
+
+	goxdr "github.com/xdrpp/goxdr/xdr"
+)
+
+// Generator generates random XDR values.
+type Generator struct {
+	// MaxBytesSize configures the upper bound limit for variable length
+	// opaque data and variable length strings
+	// https://tools.ietf.org/html/rfc4506#section-4.10
+	MaxBytesSize uint32
+	// MaxVecLen configures the upper bound limit for variable length arrays
+	// https://tools.ietf.org/html/rfc4506#section-4.13
+	MaxVecLen uint32
+	// Source is the rand.Source which is used by the Generator to create
+	// random values
+	Source rand.Source
+}
+
+const (
+	// DefaultMaxBytesSize is the MaxBytesSize value in the Generator returned by NewGenerator()
+	DefaultMaxBytesSize = 1024
+	// DefaultMaxVecLen is the MaxVecLen value in the Generator returned by NewGenerator()
+	DefaultMaxVecLen = 10
+	// DefaultSeed is the seed for the Source value in the Generator returned by NewGenerator()
+	DefaultSeed = 99
+)
+
+// NewGenerator returns a new Generator instance configured with default settings.
+// The returned Generator is deterministic but it is not thread-safe.
+func NewGenerator() Generator {
+	return Generator{
+		MaxBytesSize: DefaultMaxBytesSize,
+		MaxVecLen:    DefaultMaxVecLen,
+		// rand.NewSource returns a source which is *not* safe for concurrent use
+		Source: rand.NewSource(DefaultSeed),
+	}
+}
+
+// Next modifies the given shape and populates it with random value fields.
+func (g Generator) Next(shape goxdr.XdrType, presets []Preset) {
+	marshaller := &randMarshaller{
+		rand:         rand.New(g.Source),
+		maxBytesSize: g.MaxBytesSize,
+		maxVecLen:    g.MaxVecLen,
+		presets:      presets,
+	}
+	marshaller.Marshal("", shape)
+}

commit 2d7d78a6fb0123802f3f63848f4b0515651ad228

Format goxdr generated file before checking diff (#3534)
74bd0ff broke the build https://app.circleci.com/pipelines/github/stellar/go/7710/workflows/2a491bec-ab8c-4c31-9b11-5d9f4102fb08/jobs/40206 because the go file generated by goxdr is not formatted correctly

Makefile
@@ -27,5 +27,5 @@ regulated-assets-approval-server:
 	$(MAKE) -C services/regulated-assets-approval-server/ docker-build
 
 gxdr/xdr_generated.go: $(XDRS)
-	go run github.com/xdrpp/goxdr/cmd/goxdr -p gxdr -enum-comments -o $@~ $(XDRS)
-	cmp $@~ $@ 2> /dev/null || mv -f $@~ $@
+	go run github.com/xdrpp/goxdr/cmd/goxdr -p gxdr -enum-comments -o $@ $(XDRS)
+	go fmt $@

gxdr/xdr_generated.go
@@ -1,4 +1,4 @@
-// Code generated by goxdr -p gxdr -enum-comments -o gxdr/xdr_generated.go~ xdr/Stellar-SCP.x xdr/Stellar-ledger-entries.x xdr/Stellar-ledger.x xdr/Stellar-overlay.x xdr/Stellar-transaction.x xdr/Stellar-types.x; DO NOT EDIT.
+// Code generated by goxdr -p gxdr -enum-comments -o gxdr/xdr_generated.go xdr/Stellar-SCP.x xdr/Stellar-ledger-entries.x xdr/Stellar-ledger.x xdr/Stellar-overlay.x xdr/Stellar-transaction.x xdr/Stellar-types.x; DO NOT EDIT.
 
 package gxdr
 