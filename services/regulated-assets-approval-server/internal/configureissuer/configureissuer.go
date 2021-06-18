af38893a6cd2c418fba39c3c12e188dd9dd6b817
Marcelo Salloum <marcelo@stellar.org>
2021-06-18 12:09:52 -0300

    Implement Setup method.

--- services/regulated-assets-approval-server/internal/configureissuer/configureissuer.go
+++ services/regulated-assets-approval-server/internal/configureissuer/configureissuer.go
@@ -1,8 +1,14 @@
 package configureissuer
 
 import (
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/support/errors"
+	"github.com/stellar/go/support/log"
 	"github.com/stellar/go/txnbuild"
 )
 
@@ -12,6 +18,97 @@ type Options struct {
 	HorizonURL          string
 	IssuerAccountSecret string
 	NetworkPassphrase   string
+}
+
+func Setup(opts Options) {
+	hClient := &horizonclient.Client{
+		HorizonURL: opts.HorizonURL,
+		HTTP:       &http.Client{Timeout: 30 * time.Second},
+	}
+
+	err := setup(opts, hClient)
+	if err != nil {
+		log.Error(errors.Wrap(err, "setting up issuer account"))
+		log.Fatal("Couldn't complete setup!")
+	}
+}
+
+func setup(opts Options, hClient horizonclient.ClientInterface) error {
+	issuerKP, err := keypair.ParseFull(opts.IssuerAccountSecret)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "parsing secret"))
+	}
+
+	issuerAcc, err := hClient.AccountDetail(horizonclient.AccountRequest{
+		AccountID: issuerKP.Address(),
+	})
+	if err != nil {
+		return errors.Wrapf(err, "getting detail for account %s", issuerKP.Address())
+	}
+	// TODO: if account doesn't exist, fund it with friendbot if in testnet.
+
+	asset := txnbuild.CreditAsset{
+		Code:   opts.AssetCode,
+		Issuer: issuerKP.Address(),
+	}
+	assetResults, err := hClient.Assets(horizonclient.AssetRequest{
+		ForAssetCode:   asset.Code,
+		ForAssetIssuer: asset.Issuer,
+		Limit:          1,
+	})
+	if err != nil {
+		return errors.Wrap(err, "getting list of assets")
+	}
+
+	u, err := url.Parse(opts.BaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	homeDomain := u.Hostname()
+
+	if issuerAcc.Flags.AuthRequired && issuerAcc.Flags.AuthRevocable && issuerAcc.HomeDomain == homeDomain && len(assetResults.Embedded.Records) > 0 {
+		log.Warn("Account already configured. Aborting without performing any action...")
+		return nil
+	}
+
+	dummyKP, opsToProduceExistingBalance, err := produceExistingBalance(asset)
+	if err != nil {
+		return errors.Wrap(err, "building operations to produce existing asset trustline")
+	}
+
+	tx, err := txnbuild.NewTransaction(txnbuild.TransactionParams{
+		SourceAccount:        &issuerAcc,
+		IncrementSequenceNum: true,
+		Operations: append(
+			[]txnbuild.Operation{
+				&txnbuild.SetOptions{
+					SetFlags: []txnbuild.AccountFlag{
+						txnbuild.AuthRequired,
+						txnbuild.AuthRevocable,
+					},
+					HomeDomain: &homeDomain,
+				},
+			},
+			opsToProduceExistingBalance...,
+		),
+		BaseFee:    300,
+		Timebounds: txnbuild.NewTimeout(300),
+	})
+	if err != nil {
+		return errors.Wrap(err, "building transaction")
+	}
+
+	tx, err = tx.Sign(opts.NetworkPassphrase, issuerKP, dummyKP)
+	if err != nil {
+		return errors.Wrap(err, "signing transaction")
+	}
+
+	_, err = hClient.SubmitTransaction(tx)
+	if err != nil {
+		return errors.Wrap(err, "submitting transaction")
+	}
+
+	return nil
 }
 
 // produceExistingBalance is used to generate one trustline with a non-empty
--- services/regulated-assets-approval-server/internal/configureissuer/configureissuer_test.go
+++ services/regulated-assets-approval-server/internal/configureissuer/configureissuer_test.go
@@ -2,10 +2,15 @@ package configureissuer
 
 import (
 	"testing"
+	"time"
 
+	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/txnbuild"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -49,4 +54,155 @@ func TestProduceExistingBalance(t *testing.T) {
 		},
 	}
 	require.Equal(t, wantOps, gotOps)
+}
+
+func TestSetup_accountAlreadyConfigured(t *testing.T) {
+	issuerKP := keypair.MustRandom()
+	opts := Options{
+		AssetCode:           "FOO",
+		BaseURL:             "https://domain.test.com/",
+		HorizonURL:          horizonclient.DefaultTestNetClient.HorizonURL,
+		IssuerAccountSecret: issuerKP.Seed(),
+		NetworkPassphrase:   network.TestNetworkPassphrase,
+	}
+
+	horizonMock := horizonclient.MockClient{}
+	horizonMock.
+		On("AccountDetail", horizonclient.AccountRequest{AccountID: issuerKP.Address()}).
+		Return(horizon.Account{
+			AccountID: issuerKP.Address(),
+			Flags: horizon.AccountFlags{
+				AuthRequired:  true,
+				AuthRevocable: true,
+			},
+			HomeDomain: "domain.test.com",
+			Sequence:   "10",
+		}, nil)
+	horizonMock.
+		On("Assets", horizonclient.AssetRequest{
+			ForAssetCode:   opts.AssetCode,
+			ForAssetIssuer: issuerKP.Address(),
+			Limit:          1,
+		}).
+		Return(horizon.AssetsPage{
+			Embedded: struct{ Records []horizon.AssetStat }{
+				Records: []horizon.AssetStat{
+					{},
+				},
+			},
+		}, nil)
+
+	err := setup(opts, &horizonMock)
+	require.NoError(t, err)
+}
+
+func TestSetup(t *testing.T) {
+	issuerKP := keypair.MustRandom()
+	opts := Options{
+		AssetCode:           "FOO",
+		BaseURL:             "https://domain.test.com/",
+		HorizonURL:          horizonclient.DefaultTestNetClient.HorizonURL,
+		IssuerAccountSecret: issuerKP.Seed(),
+		NetworkPassphrase:   network.TestNetworkPassphrase,
+	}
+
+	horizonMock := horizonclient.MockClient{}
+	horizonMock.
+		On("AccountDetail", horizonclient.AccountRequest{AccountID: issuerKP.Address()}).
+		Return(horizon.Account{
+			AccountID: issuerKP.Address(),
+			Sequence:  "10",
+		}, nil)
+	horizonMock.
+		On("Assets", horizonclient.AssetRequest{
+			ForAssetCode:   opts.AssetCode,
+			ForAssetIssuer: issuerKP.Address(),
+			Limit:          1,
+		}).
+		Return(horizon.AssetsPage{
+			Embedded: struct{ Records []horizon.AssetStat }{
+				Records: []horizon.AssetStat{},
+			},
+		}, nil)
+	horizonMock.
+		On("SubmitTransaction", mock.AnythingOfType("*txnbuild.Transaction")).
+		Run(func(args mock.Arguments) {
+			tx, ok := args.Get(0).(*txnbuild.Transaction)
+			require.True(t, ok)
+			issuerSimpleAcc := txnbuild.SimpleAccount{
+				AccountID: issuerKP.Address(),
+				Sequence:  11,
+			}
+			assert.Equal(t, issuerSimpleAcc, tx.SourceAccount())
+			assert.Equal(t, int64(11), tx.SequenceNumber())
+			assert.Equal(t, int64(300), tx.BaseFee())
+			assert.Equal(t, int64(0), tx.Timebounds().MinTime)
+			assert.LessOrEqual(t, time.Now().UTC().Unix()+299, tx.Timebounds().MaxTime)
+			assert.GreaterOrEqual(t, time.Now().UTC().Unix()+301, tx.Timebounds().MaxTime)
+
+			createAccOp, ok := tx.Operations()[1].(*txnbuild.CreateAccount)
+			require.True(t, ok)
+			dummyAccAddress := createAccOp.Destination
+			homeDomain := "domain.test.com"
+			testAsset := txnbuild.CreditAsset{
+				Code:   opts.AssetCode,
+				Issuer: issuerKP.Address(),
+			}
+
+			wantOps := []txnbuild.Operation{
+				&txnbuild.SetOptions{
+					SetFlags: []txnbuild.AccountFlag{
+						txnbuild.AuthRequired,
+						txnbuild.AuthRevocable,
+					},
+					HomeDomain: &homeDomain,
+				},
+				&txnbuild.CreateAccount{
+					Destination:   dummyAccAddress,
+					Amount:        "1.5",
+					SourceAccount: testAsset.Issuer,
+				},
+				&txnbuild.ChangeTrust{
+					Line:          testAsset,
+					SourceAccount: dummyAccAddress,
+					Limit:         "922337203685.4775807",
+				},
+				&txnbuild.AllowTrust{
+					Trustor:       dummyAccAddress,
+					Authorize:     true,
+					SourceAccount: testAsset.Issuer,
+					Type:          testAsset,
+				},
+				&txnbuild.Payment{
+					Destination:   dummyAccAddress,
+					Amount:        "0.0000001",
+					Asset:         testAsset,
+					SourceAccount: testAsset.Issuer,
+				},
+				&txnbuild.AllowTrust{
+					Trustor:       dummyAccAddress,
+					Authorize:     false,
+					SourceAccount: testAsset.Issuer,
+					Type:          testAsset,
+				},
+			}
+			require.Equal(t, wantOps[1:], tx.Operations()[1:])
+			// SetOptions operation is validated separatedly because the value returned from tx.Operations()[0] contains unexported fields that prevent a proper comparision.
+			require.Equal(t, wantOps[0].(*txnbuild.SetOptions).SetFlags, tx.Operations()[0].(*txnbuild.SetOptions).SetFlags)
+			require.Equal(t, wantOps[0].(*txnbuild.SetOptions).HomeDomain, tx.Operations()[0].(*txnbuild.SetOptions).HomeDomain)
+
+			txHash, err := tx.Hash(opts.NetworkPassphrase)
+			require.NoError(t, err)
+
+			err = issuerKP.Verify(txHash[:], tx.Signatures()[0].Signature)
+			require.NoError(t, err)
+			dummyKp, err := keypair.ParseAddress(dummyAccAddress)
+			require.NoError(t, err)
+			err = dummyKp.Verify(txHash[:], tx.Signatures()[1].Signature)
+			require.NoError(t, err)
+		}).
+		Return(horizon.Transaction{}, nil)
+
+	err := setup(opts, &horizonMock)
+	require.NoError(t, err)
 }
