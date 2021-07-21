07049a3e2fe5162c42b3257778419d6b7a756917
Leigh McCulloch <leigh@stellar.org>
2021-07-21 17:31:58 +0000

    CAP-21: Disable static checks firing on xdr generated code

--- staticcheck.sh
+++ staticcheck.sh
@@ -14,7 +14,7 @@ printf "Running staticcheck...\n"
 
 ls -d */ \
   | egrep -v '^vendor|^docs' \
-  | xargs -I {} staticcheck -tests=false -checks="all,-ST1003,-SA1019,-ST1005,-ST1000,-ST1016,-S1039,-ST1021,-ST1020,-ST1019,-SA4022" ./{}...
+  | xargs -I {} staticcheck -tests=false -checks="all,-U1000,-S1005,-ST1003,-SA1019,-ST1005,-ST1000,-ST1016,-S1039,-ST1021,-ST1020,-ST1019,-SA4022" ./{}...
 
 
 # Check horizon for unused exported symbols (relying on the fact that it should be self-contained)
