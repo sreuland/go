6960b818bbef4a5775e9345ad785874669314489
Leigh McCulloch <leigh@stellar.org>
2021-07-21 17:14:23 +0000

    CAP-21: Fix test

--- network/main_test.go
+++ network/main_test.go
@@ -39,7 +39,9 @@ func TestHashTransaction(t *testing.T) {
 		Memo:          txe.Memo(),
 		Operations:    txe.Operations(),
 		SeqNum:        xdr.SequenceNumber(txe.SeqNum()),
-		TimeBounds:    txe.TimeBounds(),
+		Cond: xdr.Preconditions{
+			Type: xdr.PreconditionTypePrecondNone,
+		},
 	}
 	actual, err = HashTransaction(tx, TestNetworkPassphrase)
 	assert.NoError(t, err)
