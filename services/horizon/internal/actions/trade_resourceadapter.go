sreuland/go

commit 883f41ff3eb763a1dd09d942e9bee676dcd39494

services/horizon: Activate breaking change in /trades on protocol upgrade (#3999)
This commit updates the `GetTradesHandler` to return `Trade` struct version from
before 2.9.0. The new 2.9.0 `Trade` struct will be returned once Protocol 18
activates.

There is a breaking change in rendering of `price` field in `Trade`: it's type
changed from `int32` to `string` (to support `int64` values in JS). While new
SDKs are backward compatible (can parse both 2.8.3 and 2.9.0 version) many
organizations haven't upgraded yet. Because of that, deploying 2.9.0 to
production could break multiple integrations. The change in this PR will give
more time to developers to upgrade their SDKs.

Given the information about current protocol version is updated every second,
there is a time window of up to 1s in which protocol will be upgraded but action
code will still render 2.8.3 version of `Trade`. Fixing this issue require a lot
of new code and it is rather unlikely that there will be trades against
liquidity pool in the first Protocol 18 ledger.

services/horizon/internal/actions/trade_resourceadapter.go
@@ -1,0 +1,67 @@
+package actions
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/stellar/go/amount"
+	protocol "github.com/stellar/go/protocols/horizon"
+	horizonContext "github.com/stellar/go/services/horizon/internal/context"
+	"github.com/stellar/go/services/horizon/internal/db2/history"
+	"github.com/stellar/go/support/render/hal"
+	"github.com/stellar/go/xdr"
+)
+
+// Populate fills out the details of a trade using a row from the history_trades
+// table. This can be removed in release after P18 upgrade.
+func PopulateTradeP17(
+	ctx context.Context,
+	dest *TradeP17,
+	row history.Trade,
+) {
+	dest.ID = row.PagingToken()
+	dest.PT = row.PagingToken()
+	dest.BaseOfferID = ""
+	if row.BaseOfferID.Valid {
+		dest.BaseOfferID = fmt.Sprintf("%d", row.BaseOfferID.Int64)
+	}
+	dest.BaseAccount = row.BaseAccount.String // Always Valid in P17
+	dest.BaseAssetType = row.BaseAssetType
+	dest.BaseAssetCode = row.BaseAssetCode
+	dest.BaseAssetIssuer = row.BaseAssetIssuer
+	dest.BaseAmount = amount.String(xdr.Int64(row.BaseAmount))
+	dest.CounterOfferID = ""
+	if row.CounterOfferID.Valid {
+		dest.CounterOfferID = fmt.Sprintf("%d", row.CounterOfferID.Int64)
+	}
+	dest.CounterAccount = row.CounterAccount.String // Always Valid in P17
+	dest.CounterAssetType = row.CounterAssetType
+	dest.CounterAssetCode = row.CounterAssetCode
+	dest.CounterAssetIssuer = row.CounterAssetIssuer
+	dest.CounterAmount = amount.String(xdr.Int64(row.CounterAmount))
+	dest.LedgerCloseTime = row.LedgerCloseTime
+	dest.BaseIsSeller = row.BaseIsSeller
+
+	if row.HasPrice() {
+		dest.Price = &protocol.Price{
+			N: int32(row.PriceN.Int64),
+			D: int32(row.PriceD.Int64),
+		}
+	}
+
+	populateTradeLinks(ctx, dest, row.HistoryOperationID)
+}
+
+func populateTradeLinks(
+	ctx context.Context,
+	dest *TradeP17,
+	opid int64,
+) {
+	lb := hal.LinkBuilder{horizonContext.BaseURL(ctx)}
+	dest.Links.Base = lb.Link("/accounts", dest.BaseAccount)
+	dest.Links.Counter = lb.Link("/accounts", dest.CounterAccount)
+	dest.Links.Operation = lb.Link(
+		"/operations",
+		fmt.Sprintf("%d", opid),
+	)
+}

services/horizon/internal/integration/protocol18_test.go
@@ -1,7 +1,10 @@
 package integration
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -758,4 +761,70 @@ func TestLiquidityPoolFailedDepositAndWithdraw(t *testing.T) {
 	tt.Equal("0.0000000", withdrawal.ReservesReceived[1].Amount)
 
 	tt.Equal("0.0000010", withdrawal.Shares)
+}
+
+// TestProtocol18TradesPriceBreakingChange checks if the breaking change in
+// `/trades` (`price.n` and `price.d` change to string) activates after the
+// protocol upgrade.
+func TestProtocol18TradesPriceBreakingChange(t *testing.T) {
+	tt := assert.New(t)
+	// Start at Protocol 17!
+	config := integration.Config{ProtocolVersion: 17}
+	itest := integration.NewTest(t, config)
+	master := itest.Master()
+
+	keys, accounts := itest.CreateAccounts(1, "1000")
+	tradeKeys, tradeAccount := keys[0], accounts[0]
+
+	itest.MustSubmitMultiSigOperations(tradeAccount, []*keypair.Full{tradeKeys, master},
+		&txnbuild.ChangeTrust{
+			Line: txnbuild.ChangeTrustAssetWrapper{
+				Asset: txnbuild.CreditAsset{
+					Code:   "USD",
+					Issuer: master.Address(),
+				},
+			},
+			Limit: txnbuild.MaxTrustlineLimit,
+		},
+		&txnbuild.ManageBuyOffer{
+			Selling: txnbuild.NativeAsset{},
+			Buying: txnbuild.CreditAsset{
+				Code:   "USD",
+				Issuer: master.Address(),
+			},
+			Amount: "500",
+			Price:  "1",
+		},
+		&txnbuild.ManageBuyOffer{
+			SourceAccount: master.Address(),
+			Selling: txnbuild.CreditAsset{
+				Code:   "USD",
+				Issuer: master.Address(),
+			},
+			Buying: txnbuild.NativeAsset{},
+			Amount: "500",
+			Price:  "1",
+		},
+	)
+
+	resp, err := http.Get(fmt.Sprintf("%strades", itest.Client().HorizonURL))
+	tt.NoError(err)
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	tt.NoError(err)
+	tt.Contains(string(body), `"n": 1,`) // Number
+	tt.Contains(string(body), `"d": 1`)  // Number
+
+	// Now upgrade to protocol 18 and update core info manually to check
+	// responses format
+	itest.UpgradeProtocol(18)
+	itest.Horizon().UpdateStellarCoreInfo(context.Background())
+
+	resp, err = http.Get(fmt.Sprintf("%strades", itest.Client().HorizonURL))
+	tt.NoError(err)
+	body, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	tt.NoError(err)
+	tt.Contains(string(body), `"n": "1",`) // String
+	tt.Contains(string(body), `"d": "1"`)  // String
 }

services/horizon/internal/actions/main.go
@@ -1,0 +1,7 @@
+package actions
+
+import "github.com/stellar/go/services/horizon/internal/corestate"
+
+type CoreStateGetter interface {
+	GetCoreState() corestate.State
+}

services/horizon/internal/actions/root.go
@@ -5,14 +5,9 @@ import (
 	"net/url"
 
 	"github.com/stellar/go/protocols/horizon"
-	"github.com/stellar/go/services/horizon/internal/corestate"
 	"github.com/stellar/go/services/horizon/internal/ledger"
 	"github.com/stellar/go/services/horizon/internal/resourceadapter"
 )
-
-type CoreStateGetter interface {
-	GetCoreState() corestate.State
-}
 
 type GetRootHandler struct {
 	LedgerState *ledger.State

services/horizon/internal/actions/trade.go
@@ -20,6 +20,41 @@ import (
 	"github.com/stellar/go/xdr"
 )
 
+// TradeP17 represents a horizon digested trade.
+// This can be removed in release after P18 upgrade.
+type TradeP17 struct {
+	Links struct {
+		Self      hal.Link `json:"self"`
+		Base      hal.Link `json:"base"`
+		Counter   hal.Link `json:"counter"`
+		Operation hal.Link `json:"operation"`
+	} `json:"_links"`
+
+	ID                 string         `json:"id"`
+	PT                 string         `json:"paging_token"`
+	LedgerCloseTime    gTime.Time     `json:"ledger_close_time"`
+	OfferID            string         `json:"offer_id"`
+	BaseOfferID        string         `json:"base_offer_id"`
+	BaseAccount        string         `json:"base_account"`
+	BaseAmount         string         `json:"base_amount"`
+	BaseAssetType      string         `json:"base_asset_type"`
+	BaseAssetCode      string         `json:"base_asset_code,omitempty"`
+	BaseAssetIssuer    string         `json:"base_asset_issuer,omitempty"`
+	CounterOfferID     string         `json:"counter_offer_id"`
+	CounterAccount     string         `json:"counter_account"`
+	CounterAmount      string         `json:"counter_amount"`
+	CounterAssetType   string         `json:"counter_asset_type"`
+	CounterAssetCode   string         `json:"counter_asset_code,omitempty"`
+	CounterAssetIssuer string         `json:"counter_asset_issuer,omitempty"`
+	BaseIsSeller       bool           `json:"base_is_seller"`
+	Price              *horizon.Price `json:"price"`
+}
+
+// PagingToken implementation for hal.Pageable
+func (res TradeP17) PagingToken() string {
+	return res.PT
+}
+
 // TradeAssetsQueryParams represents the base and counter assets on trade related end-points.
 type TradeAssetsQueryParams struct {
 	BaseAssetType      string `schema:"base_asset_type" valid:"assetType,optional"`
@@ -152,6 +187,7 @@ func (q TradesQuery) Validate() error {
 // GetTradesHandler is the action handler for all end-points returning a list of trades.
 type GetTradesHandler struct {
 	LedgerState *ledger.State
+	CoreStateGetter
 }
 
 // GetResourcePage returns a page of trades.
@@ -207,10 +243,20 @@ func (handler GetTradesHandler) GetResourcePage(w HeaderWriter, r *http.Request)
 	}
 
 	var response []hal.Pageable
+	protocolVersion := handler.GetCoreState().CurrentProtocolVersion
 	for _, record := range records {
-		var res horizon.Trade
-		resourceadapter.PopulateTrade(ctx, &res, record)
-		response = append(response, res)
+		if protocolVersion >= 18 {
+			var res horizon.Trade
+			resourceadapter.PopulateTrade(ctx, &res, record)
+			response = append(response, res)
+		} else {
+			// This can be removed in release after P18 upgrade and is here
+			// to remove breaking change before protocol upgrade.
+			var res TradeP17
+			PopulateTradeP17(ctx, &res, record)
+			response = append(response, res)
+		}
+
 	}
 
 	return response, nil

services/horizon/internal/actions_root_test.go
@@ -104,6 +104,6 @@ func TestRootCoreClientInfoErrored(t *testing.T) {
 		var actual horizon.Root
 		err := json.Unmarshal(w.Body.Bytes(), &actual)
 		ht.Require.NoError(err)
-		ht.Assert.Equal(int32(0), actual.CurrentProtocolVersion)
+		ht.Assert.Equal(int32(18), actual.CurrentProtocolVersion)
 	}
 }

services/horizon/internal/actions_trade_test.go
@@ -5,14 +5,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/guregu/null"
-	"github.com/stellar/go/amount"
-	"github.com/stellar/go/keypair"
 	"net/url"
 	"strconv"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/guregu/null"
+	"github.com/stellar/go/amount"
+	"github.com/stellar/go/keypair"
 
 	"github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/services/horizon/internal/db2/history"

services/horizon/internal/httpt_test.go
@@ -34,15 +34,17 @@ func startHTTPTest(t *testing.T, scenario string) *HTTPT {
 			"network": "test",
 			"build": "test-core",
 			"ledger": {
-				"version": 13,
+				"version": 18,
 				"num": 64
 			},
-			"protocol_version": 4
+			"protocol_version": 18,
+			"network": "Test SDF Network ; September 2015"
 		}
 	}`)
 
 	ret.App.config.StellarCoreURL = ret.coreServer.URL
 	ret.App.UpdateCoreLedgerState(context.Background())
+	ret.App.UpdateStellarCoreInfo(context.Background())
 	ret.App.UpdateHorizonLedgerState(context.Background())
 
 	return ret

commit f202068d02204dab676b34f841beab67dcfa3f23

services/horizon: Fix empty offer_id in /trades before Protocol 18 (#4001)
Fixes a bug introduced in 883f41f. `offer_id` was empty but in Horizon 2.8.3
it's not empty. Also fixed a bug in `horizon-cmp`. We want `/trades` responses
to be exactly the same as in 2.8.3 to avoid breaking changes. Fortunately,
`offer_id` can be derived from existing DB fields even though it was removed in
2.9.0 migrations.

Code imports `processors` in `actions` package. This is to avoid refactoring and
this PR will be reverted after Protocol 18 vote anyway.

services/horizon/internal/actions/trade_resourceadapter.go
@@ -8,6 +8,7 @@ import (
 	protocol "github.com/stellar/go/protocols/horizon"
 	horizonContext "github.com/stellar/go/services/horizon/internal/context"
 	"github.com/stellar/go/services/horizon/internal/db2/history"
+	"github.com/stellar/go/services/horizon/internal/ingest/processors"
 	"github.com/stellar/go/support/render/hal"
 	"github.com/stellar/go/xdr"
 )
@@ -41,6 +42,13 @@ func PopulateTradeP17(
 	dest.CounterAmount = amount.String(xdr.Int64(row.CounterAmount))
 	dest.LedgerCloseTime = row.LedgerCloseTime
 	dest.BaseIsSeller = row.BaseIsSeller
+
+	_, counterOfferIDType := processors.DecodeOfferID(row.CounterOfferID.Int64)
+	if counterOfferIDType == processors.CoreOfferIDType {
+		dest.OfferID = fmt.Sprintf("%d", row.CounterOfferID.Int64)
+	} else {
+		dest.OfferID = fmt.Sprintf("%d", row.BaseOfferID.Int64)
+	}
 
 	if row.HasPrice() {
 		dest.Price = &protocol.Price{

tools/horizon-cmp/internal/response.go
@@ -35,7 +35,7 @@ var removeRegexps = []*regexp.Regexp{
 	// regexp.MustCompile(`\s*"public_key": "G.*",`),
 	// regexp.MustCompile(`,\s*"paging_token": ?""`),
 	// Removes last_modified_time field, introduced in horizon 1.3.0
-	regexp.MustCompile(`\s*"last_modified_time": ?"[^"]*",`),
+	// regexp.MustCompile(`\s*"last_modified_time": ?"[^"]*",`),
 }
 
 type replace struct {
@@ -45,38 +45,37 @@ type replace struct {
 
 var replaceRegexps = []replace{
 	// Offer ID in /offers
-	{regexp.MustCompile(`"id":( ?)([0-9]+)`), `"id":${1}"${2}"`},
-	{regexp.MustCompile(`"offer_id":( ?)([0-9]+)`), `"offer_id":${1}"${2}"`},
-	{regexp.MustCompile(`"timestamp":( ?)([0-9]+)`), `"timestamp":${1}"${2}"`},
-	{regexp.MustCompile(`"trade_count":( ?)([0-9]+)`), `"trade_count":${1}"${2}"`},
-	{regexp.MustCompile(`"type":( ?)"manage_offer",`), `"type":${1}"manage_sell_offer",`},
-	{regexp.MustCompile(`"type":( ?)"path_payment",`), `"type":${1}"path_payment_strict_receive",`},
-	{regexp.MustCompile(`"type":( ?)"create_passive_offer",`), `"type":${1}"create_passive_sell_offer",`},
-	{regexp.MustCompile(
-		// Removes paging_token from /accounts/*
-		`"data":( ?){([^}]*)},\s*"paging_token":( ?)"([0-9A-Z]*)"`),
-		`"data":${1}{${2}},"paging_token":${3}""`,
-	},
-	{regexp.MustCompile(
-		// fee_charged is a string since horizon 1.3.0
-		`"fee_charged":( ?)([\d]+),`),
-		`"fee_charged":${1}"${2}",`,
-	},
-	{regexp.MustCompile(
-		// max_fee is a string since horizon 1.3.0
-		`"max_fee":( ?)([\d]+),`),
-		`"max_fee":${1}"${2}",`,
-	},
-	// Removes trailing SSE data, fixed in horizon 1.7.0
-	{regexp.MustCompile(
-		`\nretry:.*\nevent:.*\ndata:.*\n`),
-		``,
-	},
-	// Removes clawback, fixed in horizon 2.1.0
-	{regexp.MustCompile(
-		`,\s*"auth_clawback_enabled":\s*false`),
-		``,
-	},
+	// {regexp.MustCompile(`"id":( ?)([0-9]+)`), `"id":${1}"${2}"`},
+	// {regexp.MustCompile(`"timestamp":( ?)([0-9]+)`), `"timestamp":${1}"${2}"`},
+	// {regexp.MustCompile(`"trade_count":( ?)([0-9]+)`), `"trade_count":${1}"${2}"`},
+	// {regexp.MustCompile(`"type":( ?)"manage_offer",`), `"type":${1}"manage_sell_offer",`},
+	// {regexp.MustCompile(`"type":( ?)"path_payment",`), `"type":${1}"path_payment_strict_receive",`},
+	// {regexp.MustCompile(`"type":( ?)"create_passive_offer",`), `"type":${1}"create_passive_sell_offer",`},
+	// {regexp.MustCompile(
+	// 	// Removes paging_token from /accounts/*
+	// 	`"data":( ?){([^}]*)},\s*"paging_token":( ?)"([0-9A-Z]*)"`),
+	// 	`"data":${1}{${2}},"paging_token":${3}""`,
+	// },
+	// {regexp.MustCompile(
+	// 	// fee_charged is a string since horizon 1.3.0
+	// 	`"fee_charged":( ?)([\d]+),`),
+	// 	`"fee_charged":${1}"${2}",`,
+	// },
+	// {regexp.MustCompile(
+	// 	// max_fee is a string since horizon 1.3.0
+	// 	`"max_fee":( ?)([\d]+),`),
+	// 	`"max_fee":${1}"${2}",`,
+	// },
+	// // Removes trailing SSE data, fixed in horizon 1.7.0
+	// {regexp.MustCompile(
+	// 	`\nretry:.*\nevent:.*\ndata:.*\n`),
+	// 	``,
+	// },
+	// // Removes clawback, fixed in horizon 2.1.0
+	// {regexp.MustCompile(
+	// 	`,\s*"auth_clawback_enabled":\s*false`),
+	// 	``,
+	// },
 }
 
 var newAccountDetailsPathWithLastestLedger = regexp.MustCompile(`^/accounts/[A-Z0-9]+/(transactions|operations|payments|effects|trades)/?`)
@@ -174,8 +173,9 @@ func NewResponse(domain, path string, stream bool) *Response {
 		strings.HasPrefix(path, "/effects") ||
 		strings.HasPrefix(path, "/transactions") ||
 		strings.Contains(path, "/trade")) {
-		response.NormalizedBody = fmt.Sprintf("Latest-Ledger: %s\n%s", resp.Header.Get("Latest-Ledger"), normalizedBody)
+		normalizedBody = fmt.Sprintf("Latest-Ledger: %s\n%s", resp.Header.Get("Latest-Ledger"), normalizedBody)
 	}
+	response.NormalizedBody = normalizedBody
 	return response
 }
 

services/horizon/internal/integration/protocol18_test.go
@@ -812,8 +812,9 @@ func TestProtocol18TradesPriceBreakingChange(t *testing.T) {
 	body, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	tt.NoError(err)
-	tt.Contains(string(body), `"n": 1,`) // Number
-	tt.Contains(string(body), `"d": 1`)  // Number
+	tt.Contains(string(body), `"offer_id": "1",`) // Contains non-empty offer_id
+	tt.Contains(string(body), `"n": 1,`)          // Number
+	tt.Contains(string(body), `"d": 1`)           // Number
 
 	// Now upgrade to protocol 18 and update core info manually to check
 	// responses format
@@ -825,6 +826,7 @@ func TestProtocol18TradesPriceBreakingChange(t *testing.T) {
 	body, err = ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	tt.NoError(err)
-	tt.Contains(string(body), `"n": "1",`) // String
-	tt.Contains(string(body), `"d": "1"`)  // String
+	tt.NotContains(string(body), `"offer_id"`) // No offer_id
+	tt.Contains(string(body), `"n": "1",`)     // String
+	tt.Contains(string(body), `"d": "1"`)      // String
 }